6fb8ddbefb8e3fcdf4361219a1045cfbefaad6be
Apurv Arya <94875571+Apurv-Arya@users.noreply.github.com>
2024-08-08 01:20:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-if [ -z $SOURCE_CODE ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Apurv-Arya/Auto-Forward-Bot-V2
-else
-  echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /Auto-Forward-Bot-V2
-fi
-cd /Auto-Forward-Bot-V2
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
 python3 main.py
